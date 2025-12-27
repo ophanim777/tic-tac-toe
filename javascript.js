a4959880bab03537c902b20cb25df7b063c2fbae
ophanim777 <rikyjo49@gmail.com>
2025-12-28 06:00:55 +0700

    add variable so if the box is already filled it cannot be overwritten

--- javascript.js
+++ javascript.js
@@ -2,5 +2,7 @@ const Gameboard = (() => {
   let board = ["", "", "", "", "", "", "", "", ""];
   const getBoard = () => board;
   const placeMark = (index, mark) => {
+    if (board[index] !== "") return false;
+
   }
 })();
