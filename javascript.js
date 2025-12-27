3f8df25df7b4209999b60ba6384b260c32de777c
ophanim777 <rikyjo49@gmail.com>
2025-12-28 06:18:09 +0700

    add variable for winning combos

--- javascript.js
+++ javascript.js
@@ -22,5 +22,10 @@ const GameController = (() => {
     let player2 = Player("Player 2", "O");
     let currentPlayer = player1;
     let gameOver = false;
+    const winningCombos = [
+  [0,1,2], [3,4,5], [6,7,8],
+  [0,3,6], [1,4,7], [2,5,8],
+  [0,4,8], [2,4,6]
+];
 
 })();
